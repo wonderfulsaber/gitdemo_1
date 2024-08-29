4c3c266bcb5b490f0f193599dad154a699a76110
wang <bluecats2010@163.com>
2024-08-29 20:17:43 +0800

    分支添加

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,5 +8,6 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("update");
         System.out.println("创建分支");
+        System.out.println("分支添加");
     }
 }
