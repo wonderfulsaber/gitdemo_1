a498331faa46f984497ad2126110ded15ca6e4e6
wang <bluecats2010@163.com>
2024-08-29 20:10:51 +0800

    创建分支

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("update");
+        System.out.println("创建分支");
     }
 }
